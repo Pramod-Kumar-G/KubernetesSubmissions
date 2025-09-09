605401ec2fade09f784b9e8978991939007d8904
Pramod Kumar <pramodg2002@gmail.com>
2025-09-09 17:00:24 +0530

    feat: Serve index.html at root path

--- the_project/index.js
+++ the_project/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+
+app.use(express.static(__dirname));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log("Server started in port", PORT));
