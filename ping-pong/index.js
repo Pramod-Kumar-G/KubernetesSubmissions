ada31f10d083706437265a9caf2e890fe82c4bf6
Pramod Kumar <pramodg2002@gmail.com>
2025-09-11 18:41:53 +0530

    init: create ping-pong app to print request count

---
+++ ping-pong/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const app = require("express")();
+
+const PORT = process.env.PORT;
+
+let count = -1;
+
+app.get("/", (req, res) => {
+  count += 1;
+  res.send(`pong ${count}`);
+});
+
+app.listen(PORT, () => console.log(`ping pong started on port ${PORT}`));
