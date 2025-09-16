f9b174e6de90dc29179ae742136a373cf81771a4
Pramod Kumar <pramodg2002@gmail.com>
2025-09-16 18:51:40 +0530

    feat: return pongs and consume it via internal request to the service

--- log-output-server/index.js
+++ log-output-server/index.js
@@ -10,11 +10,15 @@ app.get("/", async (req, res) => {
   const logData = await fs.readFile("/mnt/shared/file.log", {
     encoding: "utf8",
   });
-  const requestLog = await fs.readFile("/mnt/data/requests.log", {
-    encoding: "utf8",
-  });
+  // const requestLog = await fs.readFile("/mnt/data/requests.log", {
+  //   encoding: "utf8",
+  // });
+  const response = await fetch("http://ping-pong-svc:1235/pingpong");
+  const pongs = await response.json();
+  const requestLog = logData + "<br/>" + pongs.data;
+  console.log(requestLog);
 
-  res.send(logData + "<br>Ping / Pongs: " + requestLog);
+  res.send(requestLog);
 });
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
--- ping-pong/index.js
+++ ping-pong/index.js
@@ -5,19 +5,19 @@ const fs = require("node:fs");
 const PORT = process.env.PORT;
 
 let count = -1;
-const writeToFile = () => {
-  fs.writeFile("/mnt/data/requests.log", `${count}`, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("file written successfully!");
-    }
-  });
-};
+// const writeToFile = () => {
+//   fs.writeFile("/mnt/data/requests.log", `${count}`, (err) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.log("file written successfully!");
+//     }
+//   });
+// };
 app.get("/pingpong", (req, res) => {
   count += 1;
-  writeToFile();
-  res.send(`pong ${count}`);
+  // writeToFile();
+  res.send({ data: `Ping / Pongs: ${count}` });
 });
 
 app.listen(PORT, () => console.log(`ping pong started on port ${PORT}`));
