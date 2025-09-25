16478871484bbd6cf77a217991950ca089d0286f
Pramod Kumar <pramodg2002@gmail.com>
2025-09-25 17:34:35 +0530

    change port for pingpong svc

--- log-output-server/index.js
+++ log-output-server/index.js
@@ -10,9 +10,6 @@ app.get("/", async (req, res) => {
   const logData = await fs.readFile("/mnt/shared/file.log", {
     encoding: "utf8",
   });
-  // const requestLog = await fs.readFile("/mnt/data/requests.log", {
-  //   encoding: "utf8",
-  // });
 
   const fileContent = await fs.readFile("/mnt/config/information.txt", {
     encoding: "utf8",
@@ -20,7 +17,7 @@ app.get("/", async (req, res) => {
   const MESSAGE = process.env.MESSAGE;
 
   console.log(fileContent, MESSAGE);
-  const response = await fetch("http://ping-pong-svc:1235/pingpong");
+  const response = await fetch("http://ping-pong-svc:3000/pingpong");
   const pongs = await response.json();
   const requestLog = `file content: ${fileContent} <br/> env variable: MESSAGE=${MESSAGE} <br/> ${logData} <br/> ${pongs.data}`;
   console.log(requestLog);
